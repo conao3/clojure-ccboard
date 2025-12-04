ff16af7789e6a4558dc53716a5a522dfb1dd88da
conao3 <conao3@gmail.com>
2025-12-04 12:17:30 +0900

    add schema.graphql

--- src/conao3/claude_code_dashboard/backend/core.cljs
+++ src/conao3/claude_code_dashboard/backend/core.cljs
@@ -1,19 +1,18 @@
 (ns conao3.claude-code-dashboard.backend.core
   (:require
-   ["@apollo/server" :refer [ApolloServer]]
-   ["@apollo/server/standalone" :refer [startStandaloneServer]]))
+   ["@apollo/server" :as apollo]
+   ["@apollo/server/standalone" :as apollo.standalone]
+   ["fs" :as fs]
+   ["path" :as path]))
 
-(def type-defs "
-  type Query {
-    hello: String
-  }
-")
+(def type-defs
+  (fs/readFileSync (path/join js/__dirname ".." ".." "resources" "schema.graphql") "utf-8"))
 
 (def resolvers
   #js {:Query #js {:hello (fn [] "Hello from Apollo Server!")}})
 
 (defn main [& _args]
-  (-> (ApolloServer. #js {:typeDefs type-defs :resolvers resolvers})
-      (startStandaloneServer #js {:listen #js {:port 4000}})
+  (-> (apollo/ApolloServer. #js {:typeDefs type-defs :resolvers resolvers})
+      (apollo.standalone/startStandaloneServer #js {:listen #js {:port 4000}})
       (.then (fn [res]
                (println (str "Server ready at " (.-url res)))))))
