305ab378f8ceb54d99adeca20b005eb4f38c5755
conao3 <conao3@gmail.com>
2025-12-08 01:43:01 +0900

    base text-sm

--- src/conao3/claude_code_dashboard/frontend/core.cljs
+++ src/conao3/claude_code_dashboard/frontend/core.cljs
@@ -89,28 +89,28 @@
   [:li {:key (:id message)}
    [:details.rounded.bg-background-layer-2.border-l-4.border-transparent
     [:summary.p-2.cursor-pointer [:code (str "Assistant: " (:messageId message))]]
-    [:pre.p-2.text-sm.font-mono.whitespace-pre-wrap.break-all
+    [:pre.p-2.font-mono.whitespace-pre-wrap.break-all
      (-> (:rawMessage message) js/JSON.parse yaml/dump)]]])
 
 (defn UserMessage [{:keys [message]}]
   [:li {:key (:id message)}
    [:details.rounded.bg-background-layer-1.border-l-4.border-accent-background
     [:summary.p-2.cursor-pointer [:code (str "User: " (:messageId message))]]
-    [:pre.p-2.text-sm.font-mono.whitespace-pre-wrap.break-all
+    [:pre.p-2.font-mono.whitespace-pre-wrap.break-all
      (-> (:rawMessage message) js/JSON.parse yaml/dump)]]])
 
 (defn UnknownMessage [{:keys [message]}]
   [:li {:key (:id message)}
    [:details.rounded.bg-notice-background.text-white.border-l-4.border-transparent
     [:summary.p-2.cursor-pointer [:code (str "Unknown: " (:messageId message))]]
-    [:pre.p-2.text-sm.font-mono.whitespace-pre-wrap.break-all
+    [:pre.p-2.font-mono.whitespace-pre-wrap.break-all
      (-> (:rawMessage message) js/JSON.parse yaml/dump)]]])
 
 (defn BrokenMessage [{:keys [message]}]
   [:li {:key (:id message)}
    [:details.rounded.bg-negative-background.text-white.border-l-4.border-transparent
     [:summary.p-2.cursor-pointer [:code (str "Broken: " (:messageId message))]]
-    [:pre.p-2.text-sm.font-mono.whitespace-pre-wrap.break-all
+    [:pre.p-2.font-mono.whitespace-pre-wrap.break-all
      (:rawMessage message)]]])
 
 (defn MessageList []
@@ -208,7 +208,7 @@
 
 (defn App []
   [:> apollo.react/ApolloProvider {:client apollo-client}
-   [FlexCol {:class "h-screen bg-background-base text-neutral-content p-8"}
+   [FlexCol {:class "h-screen bg-background-base text-neutral-content text-base md:text-sm p-8"}
     [:h1 {:class "text-3xl font-bold mb-6 text-accent-content"} "Claude Code Dashboard"]
     [:div.flex-1.min-h-0
      [:f> ProjectList]]]])
